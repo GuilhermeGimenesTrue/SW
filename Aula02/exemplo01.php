21f1ab7e503e47a7405a8d536c5c8b1a74fb8e01
GuilhermeGimenesWork <160783030+GuilhermeGimenesTrue@users.noreply.github.com>
2025-02-13 09:00:43 -0300

    Add files via upload

---
+++ Aula02/exemplo01.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exemplo01</title>
+</head>
+<body>
+    <?php 
+    
+        $s = "Guilherme";
+
+        echo "<p>Bem-vindo $s </p>";
+        echo "<p>Bem-vindo ". $s ."</p>"
+
+    ?>
+
+    <p>Sim<p>
+    
+    <?php echo "<p>Sim<p>"?>
+</body>
+</html>
