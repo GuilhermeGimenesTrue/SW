fae0d341a8a7a30351916236689a5566aac8f1e1
GuilhermeGimenesWork <160783030+GuilhermeGimenesTrue@users.noreply.github.com>
2025-02-27 08:54:57 -0300

    Add files via upload

---
+++ Aula03/processa01.php
@@ -0,0 +1,9 @@
+<?php 
+
+    $nome = $_REQUEST['xxtnome'];
+    $idade = $_REQUEST['xxtidade'];
+
+    echo "Nome preenchido: $nome <br>";
+    echo "Idade preenchida: $idade <br>";
+
+?>
---
+++ Aula03/processa_exe01.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $nome = $_POST['xxtnome'];
+    $idade = $_POST['xxtidade'];
+
+    echo "Nome preenchido: $nome <br>";
+    echo "Idade preenchida: $idade <br>";
+
+
+?>
