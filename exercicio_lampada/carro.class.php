8ffe8c3a0ee3fa65ffd630d7891b3bbbd9ef42bb
GuilhermeGimenesTrue <guilherme.gimenes2606909@gmail.com>
2025-05-13 16:34:37 -0300

    acabeikkkkk

--- exercicio_lampada/carro.class.php
+++ exercicio_lampada/carro.class.php
@@ -8,13 +8,13 @@
         public $Capacidade_tanque;
 
         public function MostrarDados(){
-            echo "A Marca do Carro é: " . $this->$Marca;
-            echo "O Modelo do Carro é: " . $this->$Modelo;
-            echo "O tipo de Combustivel do Carro é: " . $this->$Combustível;
-            echo "A quantidade de portas do Carro é: " . $this->$Portas;
-            echo "A Capacidade no tanque do Carro é: " . $this->$Capacidae_tanque;
+            echo "A Marca do Carro é: " . $this->$Marca."<br>";
+            echo "O Modelo do Carro é: " . $this->$Modelo."<br>";
+            echo "O tipo de Combustivel do Carro é: " . $this->$Combustível."<br>";
+            echo "A quantidade de portas do Carro é: " . $this->$Portas."<br>";
+            echo "A Capacidade no tanque do Carro é: " . $this->$Capacidae_tanque."<br>";
         }
 
-    }
+    };
 
 ?>
--- exercicio_lampada/exercicio2.php
+++ exercicio_lampada/exercicio2.php
@@ -13,5 +13,34 @@
     $carro1->Portas = 4;
     $carro1->Capacidade_tanque = 500;
 
+    $carro2->Marca = "Fiat";
+    $carro2->Modelo = "Uno";
+    $carro2->Combustivel = "Gasolina";
+    $carro2->Portas = 4;
+    $carro2->Capacidade_tanque = 300;
+
+    $carro3->Marca = "Fiat";
+    $carro3->Modelo = "Siena";
+    $carro3->Combustivel = "Gasolina";
+    $carro3->Portas = 4;
+    $carro3->Capacidade_tanque = 300;
+
+    $carro4->Marca = "Honda";
+    $carro4->Modelo = "New City Hatchback";
+    $carro4->Combustivel = "Etanol";
+    $carro4->Portas = 4;
+    $carro4->Capacidade_tanque = 500;
+
+    $carro5->Marca = "Chevrolet";
+    $carro5->Modelo = "S10";
+    $carro5->Combustivel = "Etanol";
+    $carro5->Portas = 4;
+    $carro5->Capacidade_tanque = 500;
+
+    $carro1->MostrarDados();
+    $carro2->MostrarDados();
+    $carro3->MostrarDados();
+    $carro4->MostrarDados();
+    $carro5->MostrarDados();
 
 ?>
